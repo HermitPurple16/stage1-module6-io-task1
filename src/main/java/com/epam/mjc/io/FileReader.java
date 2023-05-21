aeb2a52aeb19a0aa70fedc2d9356d449242683b5
HermitPurple16 <zhanakimov16@gmail.com>
2023-05-21 21:34:44 +0600

    bb

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -7,30 +7,23 @@ public class FileReader {
 
     public static int getLinesCount(File file) {
         int linesCount = 0;
-
+        String noNullStr = null;
         try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
-            String noNullStr = null;
             while ((noNullStr = reader.readLine()) != null) {
                 linesCount++;
             }
         } catch (IOException e) {
+            System.out.println(noNullStr);
             e.printStackTrace();
         }
         return linesCount;
     }
 
-    public Profile getDataFromFile(File file) {
+    public static String[] getResults(File file, java.io.FileReader fr) {
 
-        String[] dataArr = new String[4];
-        java.io.FileReader fr = null;
-        BufferedReader buffer = null;
-        try {
-            fr = new java.io.FileReader(file);
-            buffer = new BufferedReader(fr);
-
-            int linesCount = getLinesCount(file);
-
-            String[] data2File = new String[linesCount];
+        int linesCount = getLinesCount(file);
+        String[] data2File = new String[linesCount];
+        try (BufferedReader buffer = new BufferedReader(fr)) {
             for (int i = 0; i < linesCount; i++) {
                 data2File[i] = buffer.readLine();
                 for (int h = 0; h < data2File[i].length(); h++) {
@@ -41,16 +34,20 @@ public class FileReader {
                     }
                 }
             }
-            dataArr = data2File;
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                fr.close();
-                buffer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        }
+        return data2File;
+    }
+
+    public Profile getDataFromFile(File file) {
+
+        String[] dataArr = new String[4];
+
+        try (java.io.FileReader fr = new java.io.FileReader(file)) {
+            dataArr = getResults(file, fr);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return new Profile(dataArr[0],
                 Integer.parseInt(dataArr[1]),
